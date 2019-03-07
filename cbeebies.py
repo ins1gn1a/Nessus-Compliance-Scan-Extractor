24e05e98388f1b6b377366b3f61993c9de6579bf
Adam <ins1gn1a@users.noreply.github.com>
2019-03-07 11:47:02 +0000

    Update cbeebies.py

--- cbeebies.py
+++ cbeebies.py
@@ -25,34 +25,52 @@ ws1.title = "Passed"
 ws2 = wb.create_sheet(title="Failed")
 ws3 = wb.create_sheet(title="Warning")
 
-passed_list = [['Result','Overview','Information','Solution','References']]
-ws1.append(passed_list[0])
+header_list = [['Result','Host','Compliance Check','Overview','Details','Solution','References']]
+ws1.append(header_list[0])
+ws2.append(header_list[0])
+ws3.append(header_list[0])
 
-for ReportHost in root.findall("./Report/ReportHost/ReportItem"):
+for ReportHost in root.findall("./Report/ReportHost"): # Loops through ReportHosts
     passed_host=passed_name=passed_description=passed_actual=passed_info=passed_result=passed_solution=passed_reference = ""
     z = []
-    for x in ReportHost:
-        if x.tag == "{http://www.nessus.org/cm}compliance-result": 
-            passed_result = x.text
-            pass
-        if x.tag == "{http://www.nessus.org/cm}compliance-info": 
-            passed_info = x.text
-            pass
-        if x.tag == "{http://www.nessus.org/cm}compliance-actual-value": 
-            passed_actual = x.text
-            pass
-        if x.tag == "{http://www.nessus.org/cm}compliance-solution": 
-            passed_solution = x.text
-            pass
-        if x.tag == "{http://www.nessus.org/cm}compliance-reference": 
-            passed_reference = x.text
-            pass
+    passed_host = (ReportHost.attrib['name'])
+    
+    previousResults = ([])
 
-    if passed_result == 'PASSED':
-        ws1.append([passed_result,passed_info,passed_actual,passed_solution,passed_reference])
-    elif passed_result == 'FAILED':
-        ws2.append([passed_result,passed_info,passed_actual,passed_solution,passed_reference])
-    elif passed_result == 'WARNING':
-        ws3.append([passed_result,passed_info,passed_actual,passed_solution,passed_reference])
+    for i in ReportHost: # Loops through reportItems
+        for x in i: # Loops through Tags in reportItems
+            if x.tag == "{http://www.nessus.org/cm}compliance-result": 
+                passed_result = x.text
+                pass
+            if x.tag == "{http://www.nessus.org/cm}compliance-check-name": 
+                passed_name = x.text
+                pass
+            if x.tag == "{http://www.nessus.org/cm}compliance-info": 
+                passed_info = x.text
+                pass
+            if x.tag == "{http://www.nessus.org/cm}compliance-actual-value": 
+                passed_actual = x.text
+                pass
+            if x.tag == "{http://www.nessus.org/cm}compliance-solution": 
+                passed_solution = x.text
+                pass
+            if x.tag == "{http://www.nessus.org/cm}compliance-reference": 
+                passed_reference = x.text
+                pass
 
+        if passed_result == 'PASSED':
+            if previousResults != ([passed_result,passed_host,passed_name,passed_info,passed_actual,passed_solution,passed_reference]):
+                ws1.append([passed_result,passed_host,passed_name,passed_info,passed_actual,passed_solution,passed_reference])
+            previousResults = [passed_result,passed_host,passed_name,passed_info,passed_actual,passed_solution,passed_reference]
+        
+        if passed_result == 'FAILED':
+            if previousResults != ([passed_result,passed_host,passed_name,passed_info,passed_actual,passed_solution,passed_reference]):
+                ws2.append([passed_result,passed_host,passed_name,passed_info,passed_actual,passed_solution,passed_reference])
+            previousResults = [passed_result,passed_host,passed_name,passed_info,passed_actual,passed_solution,passed_reference]  
+
+        if passed_result == 'WARNING':
+            if previousResults != ([passed_result,passed_host,passed_name,passed_info,passed_actual,passed_solution,passed_reference]):
+                ws3.append([passed_result,passed_host,passed_name,passed_info,passed_actual,passed_solution,passed_reference])
+            previousResults = [passed_result,passed_host,passed_name,passed_info,passed_actual,passed_solution,passed_reference]              
+            
 wb.save(filename = dest_filename)
